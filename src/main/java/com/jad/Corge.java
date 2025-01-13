0e1d9e502a18a6f349acfff13575e9ab026411b3
francis_sombouth <90313805+francissombouth@users.noreply.github.com>
2025-01-13 10:05:22 +0100

    test

--- src/main/java/com/jad/Corge.java
+++ src/main/java/com/jad/Corge.java
@@ -1,10 +1,13 @@
 package com.jad;
 
 public class Corge {
+    private Foo foo;
     public Corge(final Foo foo) {
+        this.foo=foo;
     }
 
     public Foo getFoo() {
+        return foo;
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
--- src/main/java/com/jad/Foo.java
+++ src/main/java/com/jad/Foo.java
@@ -3,22 +3,35 @@ package com.jad;
 import java.util.List;
 
 public class Foo {
+    private Bar bar;
+    private List<Baz> bazs;
+    private Qux qux;
+    private Corage corge;
+    private List<Grault> graults;
+
     public Foo(final Bar bar) {
+        this.bar=bar;
+        this.bazs= new ArrayList<>();
+        this.graults=new ArrayList<>()
     }
 
     public Bar getBar() {
+        return bar;
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
     public List<Baz> getBazs() {
+        return bazs;
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
     public void addBaz(final Baz baz) {
+        bazs.add(baz);
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
     public Qux getQux() {
+        return qux;
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
@@ -27,14 +40,18 @@ public class Foo {
     }
 
     public void addGrault() {
+        Grault grault= new Grault(this);
+        graults.add(grault);
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
     public Corge getCorge() {
+        return corge
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
     public void setCorge(final Corge firstCorge) {
+        this.corge= firstCorge;
         throw new UnsupportedOperationException("Not implemented yet");
     }
 }
